b1ba33cd2e947ce5f374c3e7a7350c8c5f6d361f
drewtjohnson <dtjohnson1995@gmail.com>
2020-10-17 15:54:39 -0400

    Updated fullscreen styles, added sign up.

--- app/src/main/java/com/example/recipeapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/recipeapp/HomeScreenActivity.kt
@@ -1,12 +1,13 @@
 package com.example.recipeapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class HomeScreenActivity : AppCompatActivity() {
+class HomeScreenActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.recipeapp
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +17,13 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btnLogin.setOnClickListener() {
-            val intent = Intent(this, HomeScreenActivity::class.java)
-            startActivity(intent)
+            val login = Intent(this, HomeScreenActivity::class.java)
+            startActivity(login)
+        }
+
+        btnSignUp.setOnClickListener() {
+            val signUp = Intent(this, SignUpActivity::class.java)
+            startActivity(signUp)
         }
     }
 }
