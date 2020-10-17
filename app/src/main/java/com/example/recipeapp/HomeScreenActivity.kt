bba3aac97a4b9174d0eac1c2e2b4df35f4ea26af
drewtjohnson <dtjohnson1995@gmail.com>
2020-10-17 15:23:33 -0400

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp/HomeScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.example.recipeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
---
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.recipeapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnLogin.setOnClickListener() {
+            val intent = Intent(this, HomeScreenActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
