9024b06ea2b3c5b4eb33034f8360d8a489be1105
drewtjohnson <dtjohnson1995@gmail.com>
2020-10-19 00:21:09 -0400

    Created Firebase sign up and sign in functionality.  TODO: Reset password functionality.

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -4,21 +4,64 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var emailET: EditText
+    private lateinit var passwordET: EditText
+
+    private lateinit var loginBtn: Button
+
+    private lateinit var resetPasswordTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailET = loginEmailAddress
+        passwordET = loginPassword
+
+        loginBtn = btnLogin
+
+        resetPasswordTV = havingTrouble
+
+        auth = FirebaseAuth.getInstance()
 
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        btnLogin.setOnClickListener() {
-            val login = Intent(this, HomeScreenActivity::class.java)
-            startActivity(login)
+        loginBtn.setOnClickListener() {
+            var email: String = emailET.text.toString()
+            var password: String = passwordET.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(this, "Enter your username and password", Toast.LENGTH_LONG).show()
+                } else {
+                    auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                        if(task.isSuccessful) {
+                            Toast.makeText(this, "Happy cooking!", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, HomeScreenActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Check your email and password", Toast.LENGTH_LONG).show()
+                        }
+                    })
+            }
         }
 
         btnSignUp.setOnClickListener() {
--- app/src/main/java/com/example/recipeapp/SignUpActivity.kt
+++ app/src/main/java/com/example/recipeapp/SignUpActivity.kt
@@ -1,12 +1,62 @@
 package com.example.recipeapp
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : Activity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var nameET: EditText
+
+    private lateinit var emailET: EditText
+    private lateinit var passwordET: EditText
+    private lateinit var confirmPasswordET: EditText
+
+    private lateinit var btnSubmit: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        auth = FirebaseAuth.getInstance()
+
+        emailET = signUpEmailAddress
+        passwordET = signUpPassword
+        confirmPasswordET = signUpPasswordConfirm
+
+        btnSubmit = btnSignUpSubmit
+
+        btnSubmit.setOnClickListener{
+            var email: String = emailET.text.toString()
+            var password: String = passwordET.text.toString()
+            var confirmPassword: String = confirmPasswordET.text.toString()
+
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+                Toast.makeText(this, "Please fill in all available fields", Toast.LENGTH_LONG).show()
+            } else if (password != confirmPassword) {
+                Toast.makeText(this, "Passwords must match", Toast.LENGTH_LONG).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this, "Successfully created your account, happy cooking!", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }
     }
 }
