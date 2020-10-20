056444fef3c9ee36eba10dd9625d8ea24d992750
DrewTJohnson <dtjohnson1995@gmail.com>
2020-10-20 16:42:39 -0400

    Added bottom navigation menu

--- app/src/main/java/com/example/recipeapp/HomeScreenActivity.kt
+++ app/src/main/java/com/example/recipeapp/HomeScreenActivity.kt
@@ -8,6 +8,11 @@ import android.transition.Transition
 import android.transition.TransitionManager
 import android.util.Property
 import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.widget.Toast
+import androidx.core.view.ViewCompat.animate
 import androidx.core.view.isVisible
 import androidx.transition.Fade
 import androidx.transition.Slide
@@ -16,9 +21,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.menu_button.*
 
 class HomeScreenActivity : Activity() {
+
+//    private val bottomNav = bottomNavigation
+    private val isUp: Boolean = false
+    private val isClickable: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_screen)
+
+        bottomNavigation.visibility = View.INVISIBLE
+        isUp
 
         cvBreakfastCard.setOnClickListener() {
             val recipeList = Intent(this, RecipeListActivity::class.java)
@@ -26,19 +39,60 @@ class HomeScreenActivity : Activity() {
         }
 
         menuButton.setOnClickListener() {
-            toggle()
+            buttonSlideDown()
+            slideUp()
+            menuCloseButton.isClickable = !isClickable
+        }
+        menuCloseButton.setOnClickListener() {
+            buttonSlideUp()
+            slideDown()
+            menuCloseButton.isClickable = isClickable
         }
     }
 
-    private fun toggle() {
-        val bottomNav : View = bottomNavigation
-        val homeView : ViewGroup = mainView
+    private fun slideUp() {
+        bottomNavigation.visibility = View.VISIBLE
 
-        var transition: Transition = Fade
-        transition.setDuration(600)
-        transition.addTarget(bottomNav)
+        val animateMenu = TranslateAnimation(
+                0F,
+                0F,
+                bottomNavigation.height.toFloat(),
+                0F)
+        animateMenu.duration = 200
+        animateMenu.fillAfter = true
+        bottomNavigation.startAnimation(animateMenu)
+    }
 
-        TransitionManager.beginDelayedTransition(homeView, transition)
-        bottomNav.visibility = View.VISIBLE
+    private fun buttonSlideDown() {
+        val animateButton = TranslateAnimation(
+                0F,
+                0F,
+                0F,
+                bottomNavigation.height.toFloat())
+        animateButton.duration = 400
+        animateButton.fillAfter = true
+        menuButton.startAnimation(animateButton)
+    }
+
+    private fun slideDown() {
+        val animate = TranslateAnimation(
+                0F,
+                0F,
+                0F,
+                bottomNavigation.height.toFloat())
+        animate.duration = 400
+        animate.fillAfter = true
+        bottomNavigation.startAnimation(animate)
+    }
+
+    private fun buttonSlideUp() {
+        val animateButton = TranslateAnimation(
+                0F,
+                0F,
+                bottomNavigation.height.toFloat(),
+                0F)
+        animateButton.duration = 200
+        animateButton.fillAfter = true
+        menuButton.startAnimation(animateButton)
     }
 }
